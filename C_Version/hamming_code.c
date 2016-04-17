07510d50b353a28c5bce46c7e627ad18b787b50c
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2016-04-17 16:50:46 +0200

    Hamming code initial version completed

--- C_Version/hamming_code.c
+++ C_Version/hamming_code.c
@@ -19,7 +19,9 @@ input
 #include <string.h>
 
 /**MACROS*/
-#define MAX_HAMMING_STRING_SIZE		20
+#define MAX_HAMMING_STRING_SIZE		64
+#define CHARACTER_TERMINATION		'\0'
+#define ASCII_CONV_ZERO				48
 
 /**Enumeration*/
 typedef enum Endianess {
@@ -38,7 +40,7 @@ typedef enum MODE {
 */
 int displayBits(char *bit_string) {
 	
-	for (int i = 0; *(bit_string + i)!='\0'; ++i)
+	for (int i = 0; *(bit_string + i) != CHARACTER_TERMINATION; ++i)
 	{
 		printf("%c", *(bit_string + i));
 	}
@@ -49,7 +51,7 @@ int displayBits(char *bit_string) {
 int messageLength(char *message,MODE mode) {
 
 	int len;
-	for (len = 0; *(message+len)!='\0'; ++len);
+	for (len = 0; *(message+len) != CHARACTER_TERMINATION; ++len);
 
 	if(mode == eDEBUG) {
 		printf("MessageLength : %d\n", len);
@@ -79,7 +81,7 @@ int generateInitialHammingString(char *input_string,char *hamming_string,Endiane
 	int i,j;
 	if(endian_type == eLittleEndian) {
 		j=0;
-		for (i = 0; *(input_string + i)!='\0'; )
+		for (i = 0; *(input_string + i) != CHARACTER_TERMINATION; )
 		{
 			if ((j+1)==closestPowerOfTwo((j+1),eEXECUTE))
 			{
@@ -92,6 +94,7 @@ int generateInitialHammingString(char *input_string,char *hamming_string,Endiane
 				i++;
 			}
 		}
+		*(hamming_string+j) = CHARACTER_TERMINATION;
 	}
 	else {
 
@@ -101,12 +104,26 @@ int generateInitialHammingString(char *input_string,char *hamming_string,Endiane
 
 int parityCalculationInHammingString(char *hamming_string,Endianess endian_type) {
 
-	int i;
+	int i,j,hamming_message_len,parity=0;
+
+	hamming_message_len = messageLength(hamming_string,eEXECUTE);
 
 	if (endian_type == eLittleEndian) {
-		for (i = 0; *(hamming_string + i)!='\0'; )
+		for (i = 0; i < hamming_message_len; i++)
 		{
-			
+			if(*(hamming_string+i) == '_') {
+				*(hamming_string+i) = '0';
+				for (j = i+1; j < hamming_message_len; ++j)
+				{
+					if (((j+1)&(i+1)) != 0)
+					{
+						 parity ^= (*(hamming_string+j) - ASCII_CONV_ZERO);
+					}
+				}
+				printf("Parity in Bit Position %d : %d\n", (i+1), parity);
+				*(hamming_string+i) = parity + ASCII_CONV_ZERO;
+				parity = 0;
+			}
 		}
 	}
 }
@@ -122,7 +139,10 @@ int hammingCodeGeneration(char *input_string, Endianess endian_type) {
 	nearest_power_of_two = closestPowerOfTwo(data_bit_length,eDEBUG);
 
 	if(endian_type == eLittleEndian) {
+		
 		generateInitialHammingString(input_string,hamming_string,eLittleEndian);
+		displayBits(hamming_string);
+		parityCalculationInHammingString(hamming_string,eLittleEndian);
 		displayBits(hamming_string);
 	}	
 	else {
@@ -133,8 +153,13 @@ int hammingCodeGeneration(char *input_string, Endianess endian_type) {
 
 int main(int argc, char const *argv[])
 {
-	/* code */
-	displayBits((char*) argv[argc-1]);
-	hammingCodeGeneration((char*) argv[argc-1],eLittleEndian);
+	
+	if(messageLength((char*)argv[argc-1],eEXECUTE)>=32) {
+		printf("Hamming Limit for program exceeded. Modify in the program for the same.\n");
+	}
+	else {
+		displayBits((char*) argv[argc-1]);
+		hammingCodeGeneration((char*) argv[argc-1],eLittleEndian);
+	}
 	return 0;
 }
