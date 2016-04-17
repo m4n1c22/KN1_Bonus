2e2d394ea647a3a93b3a60297d909200f98832d9
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2016-04-17 23:18:23 +0200

    interfaces and core functions isolated.

--- C_Version/hamming_code.c
+++ C_Version/hamming_code.c
@@ -29,20 +29,16 @@ int hammingCodeGeneration(char *input_string, Endianess endian_type) {
 
 	char hamming_string[MAX_HAMMING_STRING_SIZE];
 
-	data_bit_length = messageLength(input_string,eEXECUTE);
+	data_bit_length = messageLength(input_string, eEXECUTE);
 
-	nearest_power_of_two = closestPowerOfTwo(data_bit_length,eDEBUG);
+	nearest_power_of_two = closestPowerOfTwo(data_bit_length, eDEBUG);
 
-	if(endian_type == eLittleEndian) {
 		
-		generateInitialHammingString(input_string,hamming_string,eLittleEndian);
-		displayBits(hamming_string);
-		parityCalculationInHammingString(hamming_string,eLittleEndian);
-		displayBits(hamming_string);
-	}	
-	else {
+	generateInitialHammingString(input_string, hamming_string, endian_type, eEXECUTE);
+	displayBits(hamming_string);
+	parityCalculationInHammingString(hamming_string, endian_type, eEXECUTE);
+	displayBits(hamming_string);
 
-	}
 	return 0;
 }
 
@@ -51,22 +47,17 @@ int hammingCodeVerification(char *input_hamming_string, Endianess endian_type) {
 
 	char verify_hamming_string[MAX_HAMMING_STRING_SIZE];
 	int error_bit;
-	if(endian_type == eLittleEndian) {
-
-		hammingStringCopyWithParityReset(input_hamming_string, verify_hamming_string);
-		displayBits(verify_hamming_string);
-		parityCalculationInHammingString(verify_hamming_string, eLittleEndian);
-		error_bit = parityVerification(input_hamming_string, verify_hamming_string, eLittleEndian);
-		if(error_bit != 0) {
-			printf("Single Bit Error in Bit Position : %d\n", error_bit);
-		}
-		else {
-			printf("No Single Bit errors\n");
-		}
+	hammingStringCopyWithParityReset(input_hamming_string, verify_hamming_string, endian_type, eEXECUTE);
+	displayBits(verify_hamming_string);
+	parityCalculationInHammingString(verify_hamming_string, endian_type, eEXECUTE);
+	error_bit = parityVerification(input_hamming_string, verify_hamming_string, endian_type, eEXECUTE);
+	if(error_bit != 0) {
+		printf("Single Bit Error in Bit Position : %d\n", error_bit);
 	}
 	else {
-
+		printf("No Single Bit errors\n");
 	}
+	
 	return 0;
 }
 
@@ -80,12 +71,12 @@ int main(int argc, char const *argv[])
 		//Hamming Code Generation
 		printf("Hamming Code Generation\n");
 		displayBits((char*) argv[argc-2]);
-		hammingCodeGeneration((char*) argv[argc-2],eLittleEndian);
+		hammingCodeGeneration((char*) argv[argc-2],eBigEndian);
 
 		printf("Hamming Code Verification\n");
 		//Hamming Code Verification
 		displayBits((char*) argv[argc-1]);
-		hammingCodeVerification((char*)argv[argc-1],eLittleEndian);
+		hammingCodeVerification((char*)argv[argc-1],eBigEndian);
 
 	}
 	return 0;
--- C_Version/hamming_code_convenience.h
+++ C_Version/hamming_code_convenience.h
@@ -12,66 +12,73 @@
 #ifndef HAMMING_CODE_CONVENIENCE_H_
 #define HAMMING_CODE_CONVENIENCE_H_
 
-
-int generateInitialHammingString(char *input_string,char *hamming_string,Endianess endian_type) {
+/** CORE FUNCTIONS */
+int generateInitialHammingStringCoreFunction(char *input_string, char *hamming_string, MODE mode) {
 
 	int i,j;
-	if(endian_type == eLittleEndian) {
-		j=0;
-		for (i = 0; *(input_string + i) != CHARACTER_TERMINATION; )
+	j=0;
+	for (i = 0; *(input_string + i) != CHARACTER_TERMINATION; )
+	{
+		if ((j+1)==closestPowerOfTwo((j+1), mode))
 		{
-			if ((j+1)==closestPowerOfTwo((j+1),eEXECUTE))
-			{
-				*(hamming_string+j) = '_';
-				j++;
-			}
-			else {
-				*(hamming_string+j) = *(input_string+i);
-				j++;
-				i++;
-			}
+			*(hamming_string+j) = '_';
+			j++;
 		}
-		*(hamming_string+j) = CHARACTER_TERMINATION;
+		else {
+			*(hamming_string+j) = *(input_string+i);
+			j++;
+			i++;
+		}
 	}
-	else {
+	*(hamming_string+j) = CHARACTER_TERMINATION;
+	
+	if(mode == eDEBUG) {
+		displayBits(hamming_string);
+	}
 
-	}
 	return 0;
 }
 
-int parityCalculationInHammingString(char *hamming_string,Endianess endian_type) {
+int parityCalculationInHammingStringCoreFunction(char *hamming_string, MODE mode) {
 
 	int i,j,hamming_message_len,parity=0;
 
-	hamming_message_len = messageLength(hamming_string,eEXECUTE);
+	hamming_message_len = messageLength(hamming_string, mode);
 
-	if (endian_type == eLittleEndian) {
-		for (i = 0; i < hamming_message_len; i++)
-		{
-			if(*(hamming_string+i) == '_') {
-				*(hamming_string+i) = '0';
-				for (j = i+1; j < hamming_message_len; ++j)
+	for (i = 0; i < hamming_message_len; i++)
+	{
+		if(*(hamming_string+i) == '_') {
+			*(hamming_string+i) = '0';
+			for (j = i+1; j < hamming_message_len; ++j)
+			{
+				if (((j+1)&(i+1)) != 0)
 				{
-					if (((j+1)&(i+1)) != 0)
-					{
-						 parity ^= (*(hamming_string+j) - ASCII_CONV_ZERO);
-					}
+					 parity ^= (*(hamming_string+j) - ASCII_CONV_ZERO);
 				}
-				printf("Parity in Bit Position %d : %d\n", (i+1), parity);
-				*(hamming_string+i) = parity + ASCII_CONV_ZERO;
-				parity = 0;
 			}
+			if(mode == eDEBUG) {
+				printf("Parity in Bit Position %d : %d\n", (i+1), parity);
+			}
+			*(hamming_string+i) = parity + ASCII_CONV_ZERO;
+			parity = 0;
 		}
 	}
+
+	if(mode == eDEBUG) {
+		displayBits(hamming_string);
+	}
+
+	return 0;
+
 }
 
-int hammingStringCopyWithParityReset(char *input_hamming_string, char *new_hamming_string) {
+int hammingStringCopyWithParityResetCoreFunction(char *input_hamming_string, char *new_hamming_string, MODE mode) {
 
 	int len,i;
-	len = messageLength(input_hamming_string,eEXECUTE);
+	len = messageLength(input_hamming_string,mode);
 	for (i = 0; i < len; ++i)
 	{
-		if((i+1)==closestPowerOfTwo((i+1),eEXECUTE)) {
+		if((i+1)==closestPowerOfTwo((i+1),mode)) {
 			*(new_hamming_string+i) = '_';
 		}
 		else {
@@ -80,25 +87,98 @@ int hammingStringCopyWithParityReset(char *input_hamming_string, char *new_hammi
 
 	}
 	*(new_hamming_string+i) = CHARACTER_TERMINATION;
+	if(mode == eDEBUG) {
+		
+		displayBits(new_hamming_string);
+	}
 	return 0;
 }
 
-int parityVerification(char *received_message, char *calculated_parity_message, Endianess endian_type) {
+int parityVerificationCoreFunction(char *received_message, char *calculated_parity_message, MODE mode) {
 
 	int len,i,error_bit=0;
-	len = messageLength(received_message,eEXECUTE);
-	if (endian_type == eLittleEndian) {
-		for (i = 0; i < len; ++i)
-		{
-			if((i+1)==closestPowerOfTwo((i+1),eEXECUTE)) {
-				if(*(received_message+i) != *(calculated_parity_message+i)) {
-					error_bit |= (i+1); 
-				}
+	len = messageLength(received_message, mode);
+	for (i = 0; i < len; ++i)
+	{
+		if((i+1)==closestPowerOfTwo((i+1), mode)) {
+			if(*(received_message+i) != *(calculated_parity_message+i)) {
+				error_bit |= (i+1); 
 			}
 		}
 	}
+	if (mode == eDEBUG)
+	{
+		printf("Error Bit Position %d\n", error_bit);
+	}
+	return error_bit;	
+}
+
+/** Interface Methods */
+int generateInitialHammingString(char *input_string, char *hamming_string, Endianess endian_type, MODE mode) {
+
+	char reverse_string[MAX_HAMMING_STRING_SIZE];
+	if(endian_type == eLittleEndian) {
+		
+		generateInitialHammingStringCoreFunction(input_string, hamming_string, mode);
+	}
 	else {
 
+		bitStringReversal(input_string, reverse_string, mode);
+		generateInitialHammingStringCoreFunction(reverse_string, hamming_string, mode);	
+		bitStringReversal(hamming_string, reverse_string, mode);
+		bitStringCopy(reverse_string, hamming_string, mode);
+	}
+	return 0;
+}
+
+int parityCalculationInHammingString(char *hamming_string,Endianess endian_type, MODE mode) {
+
+	char reverse_string[MAX_HAMMING_STRING_SIZE];
+	if (endian_type == eLittleEndian) {
+		parityCalculationInHammingStringCoreFunction(hamming_string, mode);
+	}
+	else {
+
+		bitStringReversal(hamming_string, reverse_string, mode);
+		parityCalculationInHammingStringCoreFunction(reverse_string, mode);	
+		bitStringReversal(reverse_string, hamming_string, mode);
+		bitStringCopy(reverse_string, hamming_string, mode);
+	}
+	return 0;
+}
+
+int hammingStringCopyWithParityReset(char *input_hamming_string, char *new_hamming_string, Endianess endian_type, MODE mode) {
+
+
+	char input_hamming_string_reverse[MAX_HAMMING_STRING_SIZE], new_hamming_string_reverse[MAX_HAMMING_STRING_SIZE];
+
+	if (endian_type == eLittleEndian) {
+
+		hammingStringCopyWithParityResetCoreFunction(input_hamming_string, new_hamming_string, mode);
+	}
+	else {
+		bitStringReversal(input_hamming_string, input_hamming_string_reverse, mode);
+		hammingStringCopyWithParityResetCoreFunction(input_hamming_string_reverse, new_hamming_string_reverse, mode);
+		bitStringReversal(new_hamming_string_reverse, new_hamming_string, mode);	
+	}
+	return 0;
+}
+
+int parityVerification(char *received_message, char *calculated_parity_message, Endianess endian_type, MODE mode) {
+
+	int error_bit=0, message_length;
+	char received_message_reverse[MAX_HAMMING_STRING_SIZE], calculated_parity_message_reverse[MAX_HAMMING_STRING_SIZE];
+
+	if (endian_type == eLittleEndian) {
+
+		error_bit = parityVerificationCoreFunction(received_message, calculated_parity_message, mode);		
+	}
+	else {
+		message_length = messageLength(received_message, mode);
+		bitStringReversal(received_message, received_message_reverse, mode);
+		bitStringReversal(calculated_parity_message, calculated_parity_message_reverse, mode);
+		error_bit = parityVerificationCoreFunction(received_message_reverse, calculated_parity_message_reverse, mode);
+		error_bit = message_length - error_bit;
 	}
 	return error_bit;	
 }
