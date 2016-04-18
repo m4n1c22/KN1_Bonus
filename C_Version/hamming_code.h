c9cb84009fdecca179b29295323b44dc21e0cbfd
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2016-04-18 17:02:43 +0200

    Core functions documentation provided.

--- C_Version/hamming_code.h
+++ C_Version/hamming_code.h
@@ -13,12 +13,14 @@
 #define HAMMING_CODE_H_
 
 /** Interface Methods */
+
+/**Interface call for the core function generateInitialHammingStringCoreFunction()*/
 int generateInitialHammingString(char *input_string, char *hamming_string, Endianess endian_type, MODE mode);
-
+/**Interface call for the core function parityCalculationInHammingStringCoreFunction()*/
 int parityCalculationInHammingString(char *hamming_string,Endianess endian_type, MODE mode);
-
+/**Interface call for the core function hammingStringCopyWithParityResetCoreFunction()*/
 int hammingStringCopyWithParityReset(char *input_hamming_string, char *new_hamming_string, Endianess endian_type, MODE mode);
-
+/**Interface call for the core function parityVerificationCoreFunction())*/
 int parityVerification(char *received_message, char *calculated_parity_message, Endianess endian_type, MODE mode);
 
 #endif
--- C_Version/hamming_code_core.c
+++ C_Version/hamming_code_core.c
@@ -14,12 +14,18 @@
 /** CORE FUNCTIONS */
 int generateInitialHammingStringCoreFunction(char *input_string, char *hamming_string, MODE mode) {
 
-	int i,j;
+	int i,j,input_string_len;
+	
 	j=0;
-	for (i = 0; *(input_string + i) != CHARACTER_TERMINATION; )
+	/**Calculating the hamming message length*/
+	input_string_len = messageLength(input_string,mode);
+	/**Loop to generate a hamming outline string with unset parity bits.*/
+	for (i = 0; i<input_string_len; )
 	{
+		/**Condition check to verify if the bit position is a parity position or not.*/
 		if ((j+1)==closestPowerOfTwo((j+1), mode))
 		{
+			/**Set the bit position as empty as indicated by the character _*/
 			*(hamming_string+j) = '_';
 			j++;
 		}
@@ -30,8 +36,9 @@ int generateInitialHammingStringCoreFunction(char *input_string, char *hamming_s
 		}
 	}
 	*(hamming_string+j) = CHARACTER_TERMINATION;
-	
+	/**To check if the mode being executed was debug or not.*/
 	if(mode == eDEBUG) {
+		/**function call to display the bitstring.*/
 		displayBits(hamming_string);
 	}
 
@@ -41,29 +48,44 @@ int generateInitialHammingStringCoreFunction(char *input_string, char *hamming_s
 int parityCalculationInHammingStringCoreFunction(char *hamming_string, MODE mode) {
 
 	int i,j,hamming_message_len,parity=0;
-
+	/**Calculating the hamming message length*/
 	hamming_message_len = messageLength(hamming_string, mode);
-
-	for (i = 0; i < hamming_message_len; i++)
-	{
+	/** Loop which calculates the parity bits values from the provided message string.*/
+	for (i = 0; i < hamming_message_len; i++) {
+		
+		/**
+			Check if the read bit position is a parity bit or not.
+			Principle used behind the parity bits unset and set is the priciple used in
+			Turing machines when played around with marking messages.
+		*/
 		if(*(hamming_string+i) == '_') {
+			/**Setting it to zero before proceeding.*/
 			*(hamming_string+i) = '0';
-			for (j = i+1; j < hamming_message_len; ++j)
-			{
-				if (((j+1)&(i+1)) != 0)
-				{
+			/**Loop which calculates the parity of the given message for the corresponding parity bit.*/
+			for (j = i+1; j < hamming_message_len; ++j) {
+				/**Check if the read bit position matches the parity bit position range.*/
+				if (((j+1)&(i+1)) != 0) {
+					 /**XORing the incoming bit position with the present parity value.*/
 					 parity ^= (*(hamming_string+j) - ASCII_CONV_ZERO);
 				}
 			}
+			/**To check if the mode being executed was debug or not.*/
 			if(mode == eDEBUG) {
+				/**Prints the resultant parity value.*/
 				printf("Parity in Bit Position %d : %d\n", (i+1), parity);
 			}
+			/**
+				Since the values are stored as characters. The resultant parity is converted to equivalent
+				character and stored in the hamming string.
+			*/
 			*(hamming_string+i) = parity + ASCII_CONV_ZERO;
+			/**temporary variable for calculatin parity is reset.*/
 			parity = 0;
 		}
 	}
-
+	/**To check if the mode being executed was debug or not.*/
 	if(mode == eDEBUG) {
+		/**function call to display the bitstring.*/
 		displayBits(hamming_string);
 	}
 
@@ -74,10 +96,13 @@ int parityCalculationInHammingStringCoreFunction(char *hamming_string, MODE mode
 int hammingStringCopyWithParityResetCoreFunction(char *input_hamming_string, char *new_hamming_string, MODE mode) {
 
 	int len,i;
+	/**Calculating the incoming hamming message length*/
 	len = messageLength(input_hamming_string,mode);
-	for (i = 0; i < len; ++i)
-	{
+	/**Loop which unsets all the parity bits in the hamming string.*/
+	for (i = 0; i < len; ++i) {
+		/**Condition to check if the read bit position is a parity bit position or not.*/
 		if((i+1)==closestPowerOfTwo((i+1),mode)) {
+			/**Set the bit position as empty as indicated by the character _ */
 			*(new_hamming_string+i) = '_';
 		}
 		else {
@@ -86,8 +111,10 @@ int hammingStringCopyWithParityResetCoreFunction(char *input_hamming_string, cha
 
 	}
 	*(new_hamming_string+i) = CHARACTER_TERMINATION;
+	
+	/**To check if the mode being executed was debug or not.*/
 	if(mode == eDEBUG) {
-		
+		/**function call to display the bitstring.*/
 		displayBits(new_hamming_string);
 	}
 	return 0;
@@ -96,18 +123,24 @@ int hammingStringCopyWithParityResetCoreFunction(char *input_hamming_string, cha
 int parityVerificationCoreFunction(char *received_message, char *calculated_parity_message, MODE mode) {
 
 	int len,i,error_bit=0;
+	/**Calculating the incoming hamming message length*/
 	len = messageLength(received_message, mode);
-	for (i = 0; i < len; ++i)
-	{
+	/**Loop which calculates the single bits error if present in the received message.*/
+	for (i = 0; i < len; ++i) {
+		/**Condition to check if the read bit position is a parity bit position or not.*/
 		if((i+1)==closestPowerOfTwo((i+1), mode)) {
+			/**Condition to check if the received parity message is different from the calculated one.*/
 			if(*(received_message+i) != *(calculated_parity_message+i)) {
+				/**If different OR/concatenate the erroroneous bit position by performing logical OR.*/
 				error_bit |= (i+1); 
 			}
 		}
 	}
-	if (mode == eDEBUG)
-	{
+	/**To check if the mode being executed was debug or not.*/
+	if (mode == eDEBUG) {
+		/**Printing the erroroneous bit position.*/
 		printf("Error Bit Position %d\n", error_bit);
 	}
+	/** Erroroneous bit position.*/
 	return error_bit;	
 }
